5a67219d696f2e3f675c6c180b335316a727c440
titouan <titouan.creach@eegle.io>
2018-02-13 18:26:34 +0100

    use Object.assign instead of the object spread operator

--- src/immutable.js
+++ src/immutable.js
@@ -1,5 +1,5 @@
 export function removeKeysFromObject(object, keys) {
   return Object.entries(object)
     .filter(([k, v]) => keys.indexOf(k) === -1)
-    .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {})
+    .reduce((acc, [k, v]) => Object.assign({}, acc, { [k]: v }), {})
 }
