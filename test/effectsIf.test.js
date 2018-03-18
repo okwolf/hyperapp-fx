8596db0eba6db42a14b77483f4f1a7ff14f40127
Wolfgang <wolf@okwolf.com>
2018-03-18 16:14:47 -0700

    removing relic of a former api

--- test/effectsIf.test.js
+++
@@ -1,8 +0,0 @@
-import { action, fxIf } from "../src"
-
-describe("fxIf", () => {
-  it("should filter out effects with truthy conditionals", () =>
-    expect(
-      fxIf([[true, action("include")], [false, action("exclude")]])
-    ).toEqual([action("include")]))
-})
