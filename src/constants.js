c664bc9ca4b52d91bda7c064ccf93b989db31a63
Wolfgang <wolf@okwolf.com>
2018-05-14 22:32:00 -0700

    update link for redux repo change

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // special property used to disambiguate action dispatch data from a new state value
-// inspired by https://github.com/reactjs/redux/blob/3b600993e91d42d1569994964e9a13606edccdf0/src/utils/actionTypes.js#L9-L14
+// inspired by https://github.com/reduxjs/redux/blob/3b600993e91d42d1569994964e9a13606edccdf0/src/utils/actionTypes.js#L9-L14
 export var INTERNAL_DISPATCH =
   "@@fx/DISPATCH" +
   Math.random()
