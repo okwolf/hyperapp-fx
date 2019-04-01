2863f469c249a02782da534696bbf36841c68376
Ruairi Mc Comb <ruairimccomb@gmail.com>
2019-04-02 00:07:42 +0200

    Update documentation

--- src/fx/History.js
+++ src/fx/History.js
@@ -11,6 +11,27 @@ function historyPushEffect(props) {
   historyPush(props.state, title, url)
 }
 
+/**
+ * Describes an effect that will update the browsers navigation history with the supplied location and state.
+ *
+ * @memberof module:fx
+ * @param {object} props
+ * @param {*} props.state - data to add to browser history
+ * @param {string} props.url - url to add to browser history
+ * @param {string} props.title - title to set document to
+ * @example
+ * import { Console } from "hyperapp-fx"
+ *
+ * export const UpdateHistory = state => [
+ *   state,
+ *   HistoryPush({
+ *     state,
+ *     title: document.title,
+ *     url: '#foo'
+ *   })
+ * ]
+ */
+
 export function HistoryPush(props) {
   return [historyPushEffect, props]
 }
