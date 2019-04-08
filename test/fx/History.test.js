244915703572d946011dfff4d40e9917b3eb7ab6
Ruairi Mc Comb <ruairimccomb@gmail.com>
2019-04-08 12:27:58 +0200

    Update test

--- test/fx/History.test.js
+++ test/fx/History.test.js
@@ -1,15 +1,19 @@
 import { runFx } from "../utils"
 import { HistoryPush } from "../../src"
 
-describe("History effect", () => {
-  it("pushing to state should update location", () => {
+describe("HistoryPush effect", () => {
+  it("pushing to state should update location and state, and increment history length", () => {
     const historyPush = HistoryPush({
       state: {},
       title: "new title",
       url: "#foo"
     })
+    expect(history.length).toEqual(1)
+    expect(history.state).toEqual(null)
     runFx(historyPush)
+    expect(history.length).toEqual(2)
     expect(location.hash).toEqual("#foo")
+    expect(history.state).toEqual({})
   })
 
   it("pushing to state should send correct params", () => {
