218f0c4299e238ac110f2245a938de0c3aa38777
rmc <ruairimccomb@gmail.com>
2019-04-09 08:31:32 +0200

    Storage effect (#41)
    Add Storage FX

---
+++ src/fx/Storage.js
@@ -0,0 +1,122 @@
+import { assign } from "../utils.js"
+
+function storageArea(area) {
+  return window[area + "Storage"] || localStorage
+}
+
+function writeToStorageEffect(props) {
+  var value = props.converter(props.value)
+  storageArea(props.area).setItem(props.key, value)
+}
+
+function readFromStorageEffect(props, dispatch) {
+  try {
+    var value = props.converter(storageArea(props.area).getItem(props.key))
+    var dispatchProps = assign({}, props.props || {})
+    dispatchProps[props.prop || "value"] = value
+    dispatch(props.action, dispatchProps)
+  } catch (error) {
+    dispatch(props.error)
+  }
+}
+
+function removeFromStorageEffect(props) {
+  storageArea(props.area).removeItem(props.key)
+}
+
+/**
+ * Describes an effect that will write a key value pair to Storage. By default the item is written to `localStorage`, to write to `sessionStorage` set the `storage` prop to `session`. Values are saved in JSON, unless a custom converter is provided.
+ *
+ * @memberof module:fx
+ * @param {object} props
+ * @param {string} props.key - Specify key to use
+ * @param {*} props.value - Value to write to storage
+ * @param {string} props.storage - Storage area to write to, can be either "session" or "local"
+ * @param {function} props.converter - Use a custom converter function to encode the value of the item
+ * @example
+ * import { WriteToStorage } from "hyperapp-fx"
+ *
+ * const SavePreferences = (state, preferences) => [
+ *   state,
+ *   WriteToStorage({
+ *     name: "preferences",
+ *     value: preferences,
+ *     storage: "local"
+ *   })
+ * ]
+ *
+ */
+
+export function WriteToStorage(props) {
+  return [
+    writeToStorageEffect,
+    assign(
+      {
+        converter: props.converter || JSON.stringify
+      },
+      props
+    )
+  ]
+}
+
+/**
+ * Describes an effect that will read the value of a key from Storage. By default the item is read from `localStorage`, to read from `sessionStorage` set the `storage` prop to `session`. Values are converted from JSON, unless a custom converter is provided.
+ *
+ * @memberof module:fx
+ * @param {object} props
+ * @param {string} props.key - Specify key to use with which to write to storage
+ * @param {*} props.action - Action to call with the value of the item in storage
+ * @param {string} props.storage - Storage area to read from, can be either "session" or "local"
+ * @param {string} props.prop - Property of the action where the value is received, defaults to "value"
+ * @param {function} props.converter - Use a custom converter function to decode the value of the item
+ * @example
+ * import { ReadFromStorage } from "hyperapp-fx"
+ *
+ * const LoadPreferences = state => [
+ *  state,
+ *  ReadFromStorage({
+ *    key: "preferences",
+ *    action: function (state, { value }) {
+ *      // this action will receive the value of the item in storage
+ *    }
+ *  })
+ * ]
+ *
+ */
+
+export function ReadFromStorage(props) {
+  return [
+    readFromStorageEffect,
+    assign(
+      {
+        converter: props.converter || JSON.parse,
+        error: props.error || function() {}
+      },
+      props
+    )
+  ]
+}
+
+/**
+ * Describes an effect that will remove a key value pair Storage. By default the item is deleted from `localStorage`, to delete from `sessionStorage` set the `storage` prop to `session`.
+ *
+ * @memberof module:fx
+ * @param {object} props
+ * @param {string} props.key - Specify key to delete from storage
+ * @param {string} props.storage - Storage area to delete from, can be either "session" or "local"
+ * @example
+ * import { RemoveFromStorage } from "hyperapp-fx"
+ *
+ * const ClearPreferences = state => [
+ *  state,
+ *  RemoveFromStorage({
+ *    key: "preferences",
+ *    storage: "local"
+ *  })
+ * ]
+ *
+ */
+
+export function RemoveFromStorage(props) {
+  return [removeFromStorageEffect, props]
+}
