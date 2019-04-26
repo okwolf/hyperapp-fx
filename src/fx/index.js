ac2dfe931586194f5bf9d44c0b564333815fb44c
Wolfgang <wolf@okwolf.com>
2019-04-25 20:00:09 -0700

    remove batch fx since native support has now shipped

--- src/fx/index.js
+++ src/fx/index.js
@@ -5,7 +5,6 @@
 export * from "./Console.js"
 export * from "./Random.js"
 export * from "./Http.js"
-export * from "./BatchFx.js"
 export * from "./Merge.js"
 export * from "./Debounce.js"
 export * from "./Throttle.js"
