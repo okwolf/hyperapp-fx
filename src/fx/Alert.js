b6bec4d0781e3066c079eafdd4773e9bcf7baede
Logan Praznik <lpraz@mts.net>
2019-10-28 20:01:31 -0500

    Fixed/clarified documentation in Alert.js

--- src/fx/Alert.js
+++ src/fx/Alert.js
@@ -3,10 +3,10 @@ function alertEffect(dispatch, message) {
 }
 
 /**
- * Describes an effect that will call [`window.alert`](https://developer.mozilla.org/en-US/docs/Web/API/Window/alert) to display a dialog window with the specified message and an "OK" button.
+ * Describes an effect that will call [`window.alert`](https://developer.mozilla.org/en-US/docs/Web/API/Window/alert) to display a dialog window with the specified message and an "OK" button which can be clicked to dismiss it.
  *
  * @memberof module:fx
- * @param {...*} message - message to display in the alert dialog
+ * @param {*} message - Message to display in the alert dialog. If not a string, will be converted to a string when displayed.
  * @example
  * import { Alert } from "hyperapp-fx"
  *
